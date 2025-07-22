b56edf9478b796d5022bbffc14b0660f1a59106a
lipverso <fellipe.alencar@maida.health>
2025-07-22 16:02:54 -0300

    adicionada palavra-chave synchronized ao metodo analisarFrase em FraseService

--- src/main/java/br/prover/controller/FraseController.java
+++ src/main/java/br/prover/controller/FraseController.java
@@ -5,8 +5,8 @@ import br.prover.service.FraseService;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-    import java.io.Serializable;
-    import java.util.Map;
+import java.io.Serializable;
+import java.util.Map;
 
     @Named("fraseController")
     @ViewScoped
@@ -16,21 +16,19 @@ import javax.inject.Named;
         private FraseService fraseService;
 
         public FraseController() {
-            System.out.println("br.prover.controller.FraseController instanciado");
+            System.out.println("FraseController instanciado");
         }
 
         private Map<String, Integer> resultado;
 
         public void analisar() {
             resultado = fraseService.analisarFrase(frase);
-        //    System.out.print("Entrou");
         }
 
         public int getQtdPalavrasDistintas() {
             return resultado != null ? resultado.size() : 0;
         }
 
-        // Getters e Setters
         public String getFrase() { return frase; }
         public void setFrase(String frase) { this.frase = frase; }
         public Map<String, Integer> getResultado() { return resultado; }
--- src/main/java/br/prover/service/FraseService.java
+++ src/main/java/br/prover/service/FraseService.java
@@ -1,13 +1,12 @@
 package br.prover.service;
 
-
 import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
 import java.util.Map;
 
 @ApplicationScoped
 public class FraseService {
-    public Map<String, Integer> analisarFrase(String frase) {
+    public synchronized Map<String, Integer> analisarFrase(String frase) {
         Map<String, Integer> mapaDePalavras = new HashMap<>();
 
         //Retorna o proprio map vazio se frase for nula ou vazia após tratamento de espaço em branco
@@ -21,13 +20,8 @@ public class FraseService {
         // Divide a frase em palavras usando espaço como separador
         String[] palavras = frase.split("\\s+");
 
-
         // Loop que irá verificar se a palavra será repetida ou não.
-        // No if, é feita uma verificado se a palavra existe no map, se existir, adiciona (e substitui)
-        // a palavra e aumenta 1 na contagem.
-        // Se não existir, mantem a palavra com apenas 1 ocorrência.
         for (String palavra : palavras) {
-            //1 - Teste
             if (mapaDePalavras.containsKey(palavra)) {
                 int contagemAtual = mapaDePalavras.get(palavra);
                 mapaDePalavras.put(palavra, contagemAtual + 1);
